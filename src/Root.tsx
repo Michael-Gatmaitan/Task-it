a2c662304ea72368e9a70e2ccc51f9b66138e184
Michael Gatmaitan <mchlgtmtn@gmail.com>
2023-05-01 20:01:46 +0800

    Root unused import fixed

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link, NavLink, Outlet, useLocation } from "react-router-dom";
 import "./components/styles/root.css";
 
